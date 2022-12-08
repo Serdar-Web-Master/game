aea8235853cfb4ebfe117512a7ca22e0cc7e7175
Serdar <serdarcholukov@yandex.com>
2022-12-08 15:31:19 +0500

    3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 import { a, b} from "./vars.js";
 
 console.log(a,b);
+
+
+const doors = document.querySelectorAll('.door');
+
+console.log(doors);
+
+
+doors.forEach(function (element) {
+   element.addEventListener("click",anim);
+})
+
+function anim () {
+   this.classList.add("door-open")
+}
