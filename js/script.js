663d07d7dad79b9e62122cd6c84815c5774698a2
Serdar <serdarcholukov@yandex.com>
2022-12-20 23:59:43 +0500

    20-12

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
-import { a, b} from "./vars.js";
-console.log(a,b);
 
 const doors = document.querySelectorAll('.door');
 
 const bodyItems = document.querySelectorAll('.body-item');
 
+const headerImg = document.querySelector('.header-img');
+
+
 bodyItems.forEach(function (element){
    element.addEventListener("click",catHere);
 })
 
+let x = 0 ;
+
+headerImg.addEventListener("click",()=>{
+   if(x==0) {
+      x = x+1;
+   }
+  else {
+   x = x - 1
+  }
+   console.log(x);
+})
+
 function catHere () {
-   this.insertAdjacentHTML("afterbegin",`<div class="cat-block">
+   console.log(x);
+   if (x==1) {
+      this.insertAdjacentHTML("afterbegin",`<div class="cat-block">
    <img class="cat-img" src="./img/cat.png" alt="">
  </div>`)
+   }
+   else {
+      this.insertAdjacentHTML("afterbegin",`<div class="cat-block">
+   <img class="cat-img" src="./img/free.png" alt="">
+ </div>`)
+   }
 }
 
 doors.forEach(function (element) {
